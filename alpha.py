850d93e4629fb063800335cb7b97397cede3e73d
keilon <kwmcglon@gmail.com>
2022-07-19 11:43:09 -0700

    defining properties ...igh

--- alpha.py
+++ alpha.py
@@ -1,27 +1,31 @@
 from random import randrange, choice
 import logging
+from pprint import pformat
+
 logging.basicConfig(level=logging.INFO)
+
 
 # Gets or creates a logger
 logger = logging.getLogger(__name__)
 
 # set log level
-logger.setLevel(logging.WARNING)
+logger.setLevel(logging.INFO)
 
 # define file handler and set formatter
 file_handler = logging.FileHandler('logfile.log')
-formatter    = logging.Formatter('%(asctime)s : %(levelname)s : %(name)s : %(message)s')
+# formatter    = logging.Formatter('%(asctime)s : %(levelname)s : %(name)s : %(message)s') # has module name, useful later
+formatter    = logging.Formatter('%(asctime)s : %(levelname)s :   %(message)s')
 file_handler.setFormatter(formatter)
 
 # add file handler to logger
 logger.addHandler(file_handler)
 
 # Logs
-logger.debug('A debug message')
-logger.info('An info message')
-logger.warning('Something is not right.')
-logger.error('A Major error has happened.')
-logger.critical('Fatal error. Cannot continue')
+# logger.debug('A debug message')
+# logger.info('An info message')
+# logger.warning('Something is not right.')
+# logger.error('A Major error has happened.')
+# logger.critical('Fatal error. Cannot continue')
 
 
 def roll(rounds):
@@ -31,47 +35,110 @@ def roll(rounds):
         return [dice, speed_die]
     return dice
 
-def get_property(position):
+def get_property(name):
     ...
 
 def get_user_balance(user):
     ...
 
 
+
 def main():
     properties = {
-        "Mediterranean Avenue",
-        "Baltic Avenue",
-        "Reading RR",
-        "Oriental Avenue",
-        "Vermont Avenue",
-        "Connecticut Avenue",
-        "St. Charles Place",
-        "Electric Company",
-        "States Avenue",
-        "Virginia Avenue",
-        "Pennsylvania RR",
-        "St. James Place",
-        "Tennessee Avenue",
-        "New York Avenue",
-        "Kentucky Avenue",
-        "Indiana Avenue",
-        "Illinois Avenue",
-        "B&O RR",
-        "Atlantic Avenue",
-        "Ventnor Avenue",
-        "Water Works",
-        "Marvin Gardens",
-        "Pacific Avenue",
-        "North Carolina Avenue",
-        "Pennsylvania Avenue",
-        "Short Line",
-        "Park Place",
-        "Boardwalk"
+            "Mediterranean Avenue": {
+                "purchase_price": 60,
+                "position": 60,
+                "rent": 60,
+                "owner": 60,
+                "tax": False,
+                "action": None
+            },
+            "Baltic Avenue": {
+                "purchase_price": 60,
+                "position": 60,
+                "rent": 60,
+                "owner": 60,
+                "tax": False,
+                "action": None
+            },
+            "Reading RR": {
+                "purchase_price": 60,
+                "position": 60,
+                "rent": 60,
+                "owner": 60,
+                "tax": False,
+                "action": None
+            },
+            "Baltic Avenue": {
+                "purchase_price": 60,
+                "position": 60,
+                "rent": 60,
+                "owner": 60,
+                "tax": False,
+                "action": None
+            },
+            "Baltic Avenue": {
+                "purchase_price": 60,
+                "position": 60,
+                "rent": 60,
+                "owner": 60,
+                "tax": False,
+                "action": None
+            },
+            "Baltic Avenue": {
+                "purchase_price": 60,
+                "position": 60,
+                "rent": 60,
+                "owner": 60,
+                "tax": False,
+                "action": None
+            },
+            "Baltic Avenue": {
+                "purchase_price": 60,
+                "position": 60,
+                "rent": 60,
+                "owner": 60,
+                "tax": False,
+                "action": None
+            },
+            "Baltic Avenue": {
+                "purchase_price": 60,
+                "position": 60,
+                "rent": 60,
+                "owner": 60,
+                "tax": False,
+                "action": None
+            },
+            "Baltic Avenue": {
+                "purchase_price": 60,
+                "position": 60,
+                "rent": 60,
+                "owner": 60,
+                "tax": False,
+                "action": None
+            },
+            "Baltic Avenue": {
+                "purchase_price": 60,
+                "position": 60,
+                "rent": 60,
+                "owner": 60,
+                "tax": False,
+                "action": None
+            },
+
+
+
+
+
     }
 
-    print(roll(rounds=1))
-    print(roll(rounds=0))
+    logger.info(roll(rounds=1))
+    logger.info(roll(rounds=0))
+
+    logger.info(pformat(properties.get("Mediterranean Avenue"), indent=4))
+    logger.info(pformat(properties.get("Baltic Avenue"), indent=4))
+
+
 
 if __name__ == '__main__':
     main()
